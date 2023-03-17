0fa68343b734f7ef5d64dec670d187b4e4b6b95a
Henrique F. Simoes <henrique.simoes@lnls.br>
2023-03-17 14:46:28 -0300

    docker: Setup docker environment
    Docker container has been configured with the Debian 11 image following
    the convention currently in place at LNLS, and its subversion has been
    chosen to be the latest one.
    
    EPICS 7.0.7 has been chosen to be the base version so that the build can
    use the latest modules from EPICS. However, `pvAccess` is not enabled,
    meaning that the used communication protocol is still Channel Access.
    This will allow us to use the `int64in` and `int64out` record types from
    EPICS 3.16+.
    
    Only the needed modules from area detector have been cloned and
    compiled, so that the resulting image contains only the necessary
    dependencies for running.
    
    Similarly, ADSupport includes several libraries which submodules might
    depend on. By default, all of them are built. However, this is not
    needed for all IOCs. Therefore, here we configure it to only build the
    required libraries, which is the `xml2` lib required by ADCore.

---
+++ docker/install_modules.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -eux
+
+# Install module from GitHub tagged versions
+install_module() {
+    github_org=$1
+    module_name=$2
+    version=$3
+    release_content="$4"
+
+    # Download repository
+    wget https://github.com/$github_org/$module_name/archive/refs/tags/R$version.tar.gz
+    tar -xvzf R$version.tar.gz
+    rm -f R$version.tar.gz
+
+    cd $module_name-R$version
+    echo -e "$release_content" > configure/RELEASE
+
+    make -j ${JOBS} install
+    make -j ${JOBS} clean
+
+    cd -
+}
+
+install_module 'epics-modules' 'asyn' $ASYN_VERSION "
+EPICS_BASE = /opt/epics-R${EPICS_BASE_VERSION}/base
+"
+
+install_module 'epics-modules' 'sscan' $SSCAN_VERSION "
+EPICS_BASE = /opt/epics-R${EPICS_BASE_VERSION}/base
+"
+
+install_module 'epics-modules' 'autosave' $AUTOSAVE_VERSION "
+EPICS_BASE = /opt/epics-R${EPICS_BASE_VERSION}/base
+"
+
+install_module 'epics-modules' 'calc' $CALC_VERSION "
+SSCAN = /opt/sscan-R${SSCAN_VERSION}
+
+EPICS_BASE = /opt/epics-R${EPICS_BASE_VERSION}/base
+"
+
+install_module 'epics-modules' 'busy' $BUSY_VERSION "
+ASYN = /opt/asyn-R${ASYN_VERSION}
+AUTOSAVE = /opt/autosave-R${AUTOSAVE_VERSION}
+BUSY = /opt/busy-R${BUSY_VERSION}
+
+EPICS_BASE = /opt/epics-R${EPICS_BASE_VERSION}/base
+"
