62b1118882a8a121dc5a112bc4f91a4320378783
Henrique F. Simoes <henrique.simoes@lnls.br>
2023-03-20 12:45:01 -0300

    app: Configure dependencies to build for acA1300-75gm
    Configuration has been done for building and connecting to a
    acA1300-75gm model directly. This is a testing configuration, since not
    all models will use the same database file, as their features vary among
    models.

--- iocBoot/iocBaslerCamera/st.cmd
+++ iocBoot/iocBaslerCamera/st.cmd
@@ -1,18 +1,20 @@
 #!../../bin/linux-x86_64/BaslerCamera
 
-#- You may have to change BaslerCamera to something else
-#- everywhere it appears in this file
+< envPaths
 
-#< envPaths
-
-## Register all support components
-dbLoadDatabase "../../dbd/BaslerCamera.dbd"
+# Register all support components
+dbLoadDatabase("${TOP}/dbd/BaslerCamera.dbd")
 BaslerCamera_registerRecordDeviceDriver(pdbbase) 
 
-## Load record instances
-#dbLoadRecords("../../db/BaslerCamera.db","user=root")
+# Set up environment variables
+epicsEnvSet("P", "${EPICS_PV_AREA_PREFIX}")
+epicsEnvSet("R", "${EPICS_PV_DEVICE_PREFIX}")
+epicsEnvSet("IP_ADDRESS", "${BASLER_IP_ADDRESS}")
+
+# Configure connection to camera
+aravisConfig("BaslerCamera", "$(IP_ADDRESS)", 0, 0, 0)
+
+# Load record instances
+dbLoadRecords("${TOP}/db/acA1300-75gm.db", "P=$(P), R=$(R), PORT=BaslerCamera")
 
 iocInit()
-
-## Start any sequence programs
-#seq sncBaslerCamera,"user=root"
