3f87a209031073bab03a30a296ad0b1240a0634d
Henrique F. Simoes <henrique.simoes@lnls.br>
2023-03-21 11:22:43 -0300

    database: Add Sirius standard PV behavior and naming
    Both the behavior and naming come from the previous implementation for
    the Basler acA1300-75gm used both at the accelerator and beamlines. This
    implementation was obtained from commit 967176553fe (latest one in any
    branch available as of now) of the following repository
    
    https://github.com/lnls-dig/basler-acA1300-75gm-epics-ioc
    
    The basleracA130075gmApp/Db/BaslerSiriusStandard.db was renamed to
    `standard.db` here. As of now, high level aliasing has not been
    included, since it shall be verified whether it is really useful in
    practice.
    
    Some records in the database use `sseq`, which is provided by the EPICS
    Calc module. Thus, it has been added as a dependency in the IOC.

--- iocBoot/iocBaslerCamera/st.cmd
+++ iocBoot/iocBaslerCamera/st.cmd
@@ -9,15 +9,21 @@
 dbLoadDatabase("${TOP}/dbd/BaslerCamera.dbd")
 BaslerCamera_registerRecordDeviceDriver(pdbbase) 
 
-## Set up environment variables
+## Set up device identification
 epicsEnvSet("P", "${EPICS_PV_AREA_PREFIX}")
 epicsEnvSet("R", "${EPICS_PV_DEVICE_PREFIX}")
 epicsEnvSet("IP_ADDRESS", "${BASLER_IP_ADDRESS}")
+epicsEnvSet("PREFIX", "$(P)$(R)")
+epicsEnvSet("PORT", "CAMPORT")
+
+## Setup overlay markers line width
+epicsEnvSet("MARK_WIDTH", 3)
 
 ## Configure connection to camera
-aravisConfig("BaslerCamera", "$(IP_ADDRESS)", 0, 0, 0)
+aravisConfig("$(PORT)", "$(IP_ADDRESS)", 0, 0, 0)
 
 ## Load record instances
-dbLoadRecords("${TOP}/db/acA1300-75gm.db", "P=$(P), R=$(R), PORT=BaslerCamera")
+dbLoadRecords("${TOP}/db/acA1300-75gm.db", "P=$(PREFIX), R=Cam1, PORT=$(PORT)")
+dbLoadRecords("${TOP}/db/standard.db", "P=$(P), R=$(R), MARK_WIDTH=$(MARK_WIDTH), PORT=$(PORT)")
 
 iocInit()
