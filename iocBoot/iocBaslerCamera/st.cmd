e983378a6bbec93edc50cb8eb2f4e96941483edc
Henrique F. Simoes <henrique.simoes@lnls.br>
2023-03-30 14:00:58 -0300

    database: Change driver and plugins prefix to use colon
    It is a pattern in areaDetector to use the plugins and driver names
    suffixed by a colon, for instance `Cam1:` instead of simply `Cam1`.
    In configuration files from the beamlines, this pattern is already used
    by rewriting the `st.cmd` file provided from the original repository.
    Such names currently only impact the aliases and forward links made in
    the database following Sirius PV naming standard, which is used only in
    the accelerator.
    
    Therefore, a downside of following this approach is having other modules
    that use directly the PVs from area detector and its plugins
    reconfigured. An alternative to solve this is using a macro that will be
    expanded based on the configuration provided. However, a better study on
    whether the PV from the standard are really needed should be done
    beforehand.

--- iocBoot/iocBaslerCamera/st.cmd
+++ iocBoot/iocBaslerCamera/st.cmd
@@ -28,13 +28,13 @@ epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db:$(ADGENICAM)/db")
 
 ## Configure camera driver
 aravisConfig("$(PORT)", "$(IP_ADDRESS)", 0, 0, 0)
-dbLoadRecords("$(ADARAVIS)/db/aravisCamera.template", "P=$(PREFIX), R=Cam1, PORT=$(PORT)")
-dbLoadRecords("${TOP}/db/acA1300-75gm.db", "P=$(PREFIX), R=Cam1, PORT=$(PORT)")
+dbLoadRecords("$(ADARAVIS)/db/aravisCamera.template", "P=$(PREFIX), R=Cam1:, PORT=$(PORT)")
+dbLoadRecords("${TOP}/db/acA1300-75gm.db", "P=$(PREFIX), R=Cam1:, PORT=$(PORT)")
 
 # Create a standard arrays plugin
 NDStdArraysConfigure("Image1", "$(QSIZE)", 0, "$(PORT)", 0, 0, 0, 0)
 # Allow for cameras up to 1280x1024x1 for monochromatic
-dbLoadRecords("NDStdArrays.template", "P=$(PREFIX), R=Image1, PORT=Image1, ADDR=0, TIMEOUT=1, NDARRAY_PORT=$(PORT), TYPE=Int16, FTVL=USHORT, NELEMENTS=1310720")
+dbLoadRecords("NDStdArrays.template", "P=$(PREFIX), R=Image1:, PORT=Image1, ADDR=0, TIMEOUT=1, NDARRAY_PORT=$(PORT), TYPE=Int16, FTVL=USHORT, NELEMENTS=1310720")
 
 ## Load Sirius standard naming compliant records
 # As they depend on the previous records, this must be done last
